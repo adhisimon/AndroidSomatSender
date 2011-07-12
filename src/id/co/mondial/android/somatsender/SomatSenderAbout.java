1d72e76f25ee743b87ae5b9a7b46bc62e5b5e6b8
Adhidarma Hadiwinoto <adhisimon@mondial.co.id>
2011-07-12 16:21:11 +0700

    prepare for SomatSenderAbout

---
+++ src/id/co/mondial/android/somatsender/SomatSenderAbout.java
@@ -0,0 +1,13 @@
+package id.co.mondial.android.somatsender;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SomatSenderAbout extends Activity {
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+    }
+}
